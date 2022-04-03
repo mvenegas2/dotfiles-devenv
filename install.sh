8164bad2ff5461bb44cd41e1be024e86d623c6de
Matt Mukerjee <mukerjee@nefeli.io>
2022-04-03 06:47:35 +0000

    updates

--- install.sh
+++ install.sh
@@ -1,25 +1,17 @@
 #!/bin/bash -ex
 
+# zsh goodies
 rm -rf ~/.oh-my-zsh
 git clone https://github.com/robbyrussell/oh-my-zsh ~/.oh-my-zsh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
+# change shell to zsh
 sudo chsh -s /bin/zsh $USER
 
+# place dotfiles
 rm -rf ~/.dotfiles
 mv ~/dotfiles ~/.dotfiles
 ln -fs ~/.dotfiles/dotsyncrc ~/.dotsyncrc
-
 cd ~/.dotfiles
 ./dotsync -I
 ./dotsync -L
-
-# goodies
-sudo apt-get update
-sudo apt-get install -y tig software-properties-common
-
-# upgrade emacs
-sudo apt-get remove 'emacs*'
-yes | sudo add-apt-repository ppa:ubuntu-elisp/ppa
-sudo apt-get update
-sudo apt-get install -y emacs-snapshot gnupg2
