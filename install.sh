03b3ff1e48b9f716c7b49a1acc1793f3114c8bff
Matt Mukerjee <mukerjee@nefeli.io>
2022-04-03 00:49:45 +0000

    make idempotent

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -ex
 
+rm -rf ~/.oh-my-zsh
 git clone https://github.com/robbyrussell/oh-my-zsh ~/.oh-my-zsh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
 sudo chsh -s /bin/zsh $USER
 
 mv ~/dotfiles ~/.dotfiles
